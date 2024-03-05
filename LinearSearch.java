1dd37dd467259dd9aa3ef21cba8dcd89e2600856
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-05 11:44:02 +0530

    code implemented for integers

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,5 +1,11 @@
 public class LinearSearch {
     public boolean linearSearch(int[] array, int search) {
-        return true;
+        for (int i = 0; i < array.length; i++) {
+            int each = array[i];
+            if (each == search) {
+                return true;
+            }
+        }
+        return false;
     }
 }
